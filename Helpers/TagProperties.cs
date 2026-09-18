1c446e5e180063a503ce0adef958f3ed530bc8ea
Harivinay003 <136343330+Harivinay003@users.noreply.github.com>
2026-09-18 11:14:33 +0530

    Add files via upload

---
+++ Helpers/TagProperties.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFSCADA.Helpers
+{
+    public static class TagProperties
+    {
+        public static readonly DependencyProperty ConfigKeyProperty =
+            DependencyProperty.RegisterAttached(
+                "ConfigKey",
+                typeof(string),
+                typeof(TagProperties),
+                new PropertyMetadata(null));
+
+        public static void SetConfigKey(
+            DependencyObject element,
+            string value)
+        {
+            element.SetValue(ConfigKeyProperty, value);
+        }
+
+        public static string GetConfigKey(
+            DependencyObject element)
+        {
+            return (string)element.GetValue(ConfigKeyProperty);
+        }
+    }
+}
